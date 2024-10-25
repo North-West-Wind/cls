d278c96bdd7e056062b51e937fe01057fa6df96b
North-West-Wind <wsyn148@gmail.com>
2024-10-25 16:44:11 +0800

    propagate key events without enter block

--- src/component/layer.rs
+++ src/component/layer.rs
@@ -2,16 +2,16 @@ use std::cmp::{max, min};
 
 use crossterm::event::{KeyCode, KeyEvent};
 
-use crate::{state::{get_app, get_mut_app, SelectionLayer}, util::threads::spawn_save_thread};
+use crate::{state::{get_mut_app, SelectionLayer}, util::threads::spawn_save_thread};
 
-use super::popup::{help::HelpPopup, quit::QuitPopup, set_popup, PopupComponent};
+use super::{block::BlockHandleKey, popup::{help::HelpPopup, quit::QuitPopup, set_popup, PopupComponent}};
 
 pub fn handle_key(event: KeyEvent) -> bool {
 	match event.code {
-		KeyCode::Up => key_navigate(0, -1),
-		KeyCode::Down => key_navigate(0, 1),
-		KeyCode::Left => key_navigate(-1, 0),
-		KeyCode::Right => key_navigate(1, 0),
+		KeyCode::Up => key_navigate(0, -1, event),
+		KeyCode::Down => key_navigate(0, 1, event),
+		KeyCode::Left => key_navigate(-1, 0, event),
+		KeyCode::Right => key_navigate(1, 0, event),
 		KeyCode::Enter => navigate_layer(false),
 		KeyCode::Char('q')|KeyCode::Esc => navigate_layer(true),
 		KeyCode::Char('?') => {
@@ -30,18 +30,21 @@ pub fn handle_key(event: KeyEvent) -> bool {
 			}
 			return true;
 		},
-		_ => false
+		_ => {
+			let app = get_mut_app();
+			app.blocks[app.block_selected as usize].handle_key(event)
+		}
 	}
 }
 
-fn key_navigate(dx: i16, dy: i16) -> bool {
+fn key_navigate(dx: i16, dy: i16, event: KeyEvent) -> bool {
 	if dx == 0 && dy == 0 { return false }
-	let app = get_app();
-	if app.selection_layer == SelectionLayer::Block {
-		navigate_block(dx, dy)
-	} else {
-		false
+	let app = get_mut_app();
+	let mut result = navigate_block(dx, dy);
+	if !result {
+		result = app.blocks[app.block_selected as usize].handle_key(event);
 	}
+	return result;
 }
 
 fn navigate_block(dx: i16, dy: i16) -> bool {
