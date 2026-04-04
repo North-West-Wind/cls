d4a746d268b5028836ad416ab9b2acbc35c6ec5c
North-West-Wind <wsyn148@gmail.com>
2026-04-04 21:27:37 +0800

    build: output sha256 checksum

--- build-all.sh
+++ build-all.sh
@@ -2,3 +2,6 @@
 
 cargo build --target x86_64-unknown-linux-gnu --release
 cargo build --target x86_64-pc-windows-gnu --release
+
+sha256sum target/x86_64-unknown-linux-gnu/release/cls
+sha256sum target/x86_64-pc-windows-gnu/release/cls.exe
